0a198c3eb569bdc71f249f951f2df9d24509b5e0
Sakline Sk <Sakline_pw1_056@masai.school>
2021-08-20 10:43:44 +0530

    This Is Exact Final Blog Page with responsive you can check and give feedback nothing to change I think

--- blog.js
+++ blog.js
@@ -1,11 +1,11 @@
-const navbar = document.querySelector('.header')
-
+const navbar = document.querySelector('.header');
 window.addEventListener('scroll', function(e) {
   const lastPosition = window.scrollY
   console.log(lastPosition);
-  if (lastPosition > 40 ) {
-    navbar.classList.add('active')
-  }  else {
+  if (lastPosition > 30 ) {
+    navbar.classList.add('active');
+  }
+  else{
     navbar.classList.remove('active')
   }
 })
